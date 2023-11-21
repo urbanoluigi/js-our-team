5b0e96a909628aec61e686d8e0ce0a20d3b7ca3c
urbanoluigi <urrbanoluigi@gmail.com>
2023-11-21 16:35:32 +0100

    array created

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/*MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite nella tabella
+qui sotto.*/
+
+
+let dipendenti = [
+    {
+        name_surname: "Wayne Barnett",
+        role: "Founder & CEO",
+        image: wayne-barnett-founder-ceo.jpg,
+    },
+    {
+        name_surname: "Angela Caroll",
+        role: "Chief Editor",
+        image: angela-caroll-chief-editor.jpg,
+    },
+    {
+        name_surname: "Walter Gordon",
+        role: "Office Manager",
+        image: walter-gordon-office-manager.jpg,
+    },
+    {
+        name_surname: "Angela Lopez",
+        role: "Social Media Manager",
+        image: angela-lopez-social-media-manager.jpg,
+    },
+    {
+        name_surname: "Scott Estrada",
+        role: "Developer",
+        image: scott-estrada-developer.jpg,
+    },
+    {
+        name_surname: "Barbara Ramos",
+        role: "Graphic",
+        image: barbara-ramos-graphic-designer.jpg,
+    },
+];
